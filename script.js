2adca5c943ad3bd7542016739f08c405136362bc
somir1 <leonsomir@gmail.com>
2021-06-29 16:21:07 -0500

    functionality works

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = document.querySelector("newname")
+
+
+function nameChange(){
+    newname.innerText = "Walter White"
+}
+
+var adrian = document.querySelector("#r1")
+var alayne = document.querySelector("#r2")
+var anne = document.querySelector("#r3")
+
+function remove(element){
+    element.remove()
+}
